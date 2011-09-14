62bed0c9a4525605d4d74ae7dd819ed98b3ea7f1
Derek Kulinski <takeda@takeda.tk>
2011-09-14 12:53:34 -0700

    forgot to add exclusions.py testcase

---
+++ tests/exclusions.py
@@ -0,0 +1,22 @@
+from pyccn import Name, Interest, _pyccn
+
+e = Interest.ExclusionFilter()
+e.add_any()
+e.add_names([Name.Name('/one'), Name.Name('/two'), Name.Name('/three'), Name.Name('/four')])
+e.add_any()
+e.add_name(Name.Name('/forty/two'))
+
+str(e)
+d = _pyccn._pyccn_ExclusionFilter_from_ccn(e.ccn_data)
+str(d)
+
+# I believe separation of /forty/two into /forty and /two is a correct behavior
+# since it doesn't make sense to have more than one level in exclusions
+result = ['<any>', '/one', '/two', '/four', '/three', '<any>', '/forty', '/two']
+
+for a,b in zip(d.components, result):
+	if str(a) == b:
+		continue
+	else:
+		raise AssertionError("%s != %s" % (str(a), b))
+
