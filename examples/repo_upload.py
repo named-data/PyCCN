e7384a10265e6a66c17919c3ccb6ce7ef60da37e
Derek Kulinski <takeda@takeda.tk>
2011-10-04 09:40:06 -0700

    moved RepoControl to pyccn.staging

---
+++ examples/repo_upload.py
@@ -0,0 +1,47 @@
+from pyccn.staging.RepoControl import RepoUpload
+from pyccn import CCN, ContentObject, Name, Key
+import struct
+
+if __name__ == '__main__':
+	def segment(segment):
+		return b'\x00' + struct.pack('!Q', segment).lstrip('\x00')
+
+	def publish(key, name, last_segment, content):
+		print("Generating: %s" % name)
+
+		# Name
+		co_name = Name.Name(name)
+
+		# SignedInfo
+		si = ContentObject.SignedInfo()
+		si.type = ContentObject.ContentType.CCN_CONTENT_DATA
+		si.finalBlockID = last_segment
+		si.publisherPublicKeyDigest = key.publicKeyID
+		si.keyLocator = Key.KeyLocator(key)
+
+		# ContentObject
+		co = ContentObject.ContentObject()
+		co.content = content
+		co.name = co_name
+		co.signedInfo = si
+
+		co.sign(key)
+		return co
+
+	name = Name.Name('/repo/upload/test')
+	name_v = Name.Name(name)
+	name_v.appendVersion()
+
+	handle = CCN.CCN()
+	key = handle.getDefaultKey()
+	last_seg = segment(9)
+
+	content = []
+	for i in range(10):
+		name_s = Name.Name(name_v)
+		name_s += segment(i)
+		co = publish(key, name_s, last_seg, "Segment: %d\n" % i)
+		content.append(co)
+
+	upload = RepoUpload(handle, name_v, content)
+	upload.start()
--- python/pyccn/staging/RepoControl.py
+++ python/pyccn/staging/RepoControl.py
@@ -1,5 +1,4 @@
-from pyccn import CCN, Closure, ContentObject, Interest, Key, Name
-import struct
+from pyccn import Closure, Interest, Name
 
 class RepoUpload(Closure.Closure):
 	def __init__(self, handle, name, content):
@@ -59,46 +58,3 @@ class RepoUpload(Closure.Closure):
 		print("name: %s" % info.Interest.name)
 		return Closure.RESULT_OK
 
-if __name__ == '__main__':
-	def segment(segment):
-		return b'\x00' + struct.pack('!Q', segment).lstrip('\x00')
-
-	def publish(key, name, last_segment, content):
-		print("Generating: %s" % name)
-
-		# Name
-		co_name = Name.Name(name)
-
-		# SignedInfo
-		si = ContentObject.SignedInfo()
-		si.type = ContentObject.ContentType.CCN_CONTENT_DATA
-		si.finalBlockID = last_segment
-		si.publisherPublicKeyDigest = key.publicKeyID
-		si.keyLocator = Key.KeyLocator(key)
-
-		# ContentObject
-		co = ContentObject.ContentObject()
-		co.content = content
-		co.name = co_name
-		co.signedInfo = si
-
-		co.sign(key)
-		return co
-
-	name = Name.Name('/repo/upload/test')
-	name_v = Name.Name(name)
-	name_v.appendVersion()
-
-	handle = CCN.CCN()
-	key = handle.getDefaultKey()
-	last_seg = segment(9)
-
-	content = []
-	for i in range(10):
-		name_s = Name.Name(name_v)
-		name_s += segment(i)
-		co = publish(key, name_s, last_seg, "Segment: %d\n" % i)
-		content.append(co)
-
-	upload = RepoUpload(handle, name_v, content)
-	upload.start()
