93cef55be6dd8bedd3b48cc709dad09c6c4a2077
Derek Kulinski <takeda@takeda.tk>
2011-10-07 22:38:59 -0700

    another directory change (can't make up my mind)

--- csrc/namecrypto/lib/namecrypto.h
+++
@@ -1,8 +0,0 @@
-//
-//  namecrypto.h
-//  namecrypto
-//
-//  Created by Paolo Gasti <pgasti@uci.edu> on 6/3/11.
-//  Copyright 2011 Paolo Gasti. All rights reserved.
-//
-
--- csrc/namecrypto/namecrypto.h
+++ csrc/namecrypto/namecrypto.h
@@ -1,12 +1,8 @@
-/*
- * Copyright (c) 2011, Regents of the University of California
- * BSD license, See the COPYING file for more information
- * Written by: Derek Kulinski <takeda@takeda.tk>
- */
+//
+//  namecrypto.h
+//  namecrypto
+//
+//  Created by Paolo Gasti <pgasti@uci.edu> on 6/3/11.
+//  Copyright 2011 Paolo Gasti. All rights reserved.
+//
 
-#ifndef _NAMECRYPTO_H_
-#  define _NAMECRYPTO_H_
-
-extern PyObject *_namecrypto_module;
-
-#endif /* _NAMECRYPTO_H_ */
