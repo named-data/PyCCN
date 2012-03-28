716da47bfc12c1e9ff7c73f5d49ecaf714761357
Derek Kulinski <takeda@takeda.tk>
2012-03-28 15:46:59 -0700

    Corrected copyright info

--- csrc/namecrypto/encryption_challenge.c
+++ csrc/namecrypto/encryption_challenge.c
@@ -3,7 +3,7 @@
 //  namecrypto
 //
 //  Created by Naveen Nathan on 12/15/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Naveen Nathan. All rights reserved.
 //
 
 #include <stdlib.h>
