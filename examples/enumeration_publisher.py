eb3cb73020f697c09edb30b72eb58453c6bb946b
Derek Kulinski <takeda@takeda.tk>
2012-07-01 21:38:01 -0700

    Example of publishing using the enumeration protocol
    Copyright note for the enumeration example

---
+++ examples/enumeration_publisher.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+#
+# Copyright (c) 2011, Regents of the University of California
+# BSD license, See the COPYING file for more information
+# Written by: Derek Kulinski <takeda@takeda.tk>
+#
+
+import sys
+import pyccn
+from pyccn.impl.enumeration import enumerate
+from pyccn.impl.segmenter import segmenter, Wrapper
+
+def generate_names():
+	names = ["/Hello", "/World", "/This", "/is", "/an", "/enumeration", "/example"]
+	return map(lambda x: pyccn.Name(x), names)
+
+def main(args):
+	if len(sys.argv) != 2:
+		usage()
+
+	name = pyccn.Name(sys.argv[1])
+	data = enumerate(generate_names())
+
+	key = pyccn.CCN.getDefaultKey()
+	name = name.append('\xc1.E.be').appendKeyID(key.publicKeyID).appendVersion()
+
+	wrapper = Wrapper(name, key)
+	sgmtr = segmenter(data, wrapper)
+
+	handle = pyccn.CCN()
+	for seg in sgmtr:
+		handle.put(seg)
+
+	return 0
+
+def usage():
+	print("Usage: %s <uri>" % sys.argv[0])
+	sys.exit(1)
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
